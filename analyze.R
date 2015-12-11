fcd9743e86b1487b606a834ea5639bb730dceee2
bgulbis <brian.gulbis@gmail.com>
2015-12-11 12:00:35 -0600

    gather all qualities

--- analyze.R
+++ analyze.R
@@ -4,6 +4,24 @@
 # 
 
 library(stringr)
+library(dplyr)
 
-low.perform <- mutate(lor, low = str_count(comment, regex("(need|room)(.*)improv", ignore_case = TRUE))) %>%
-    filter(low > 0)
+low.perform <- lor %>%
+    mutate(improve = str_count(comment, regex("(need|room|could|area)(s|ed|ing)?( +[^ ]+){0,5} improve", ignore_case = TRUE)), 
+           struggle = str_count(comment, regex("struggle", ignore_case = TRUE)),
+           concern = str_count(comment, regex("concern", ignore_case = TRUE)),
+           difficult = str_count(comment, regex("difficult", ignore_case = TRUE))) 
+
+test <- grep("improve", lor$comment, ignore.case = TRUE, value = TRUE)
+
+# did/continue to/consistently improve; big/huge/great/significant improvement; improve(d) greatly/quickly
+# will succeed; self-motivated; without prompt(ing/ed); seek(s/ing) out ways; independent
+
+# will benefit from residency; adequate
+
+# unfortunately; never/not consistent; did not/could not
+# needed/required guidance
+
+assess <- low.perform %>%
+    group_by(cas_id) %>%
+    summarise_each(funs(sum(., na.rm = TRUE)), improve:difficult)
--- tidy_data.R
+++ tidy_data.R
@@ -14,62 +14,27 @@ references <- select(ref.data, cas_id:evaluator_id_for_references_3) %>%
     group_by(cas_id, writer_id) %>%
     distinct
 
-# gather the time mangement ratings into long data format, remove those rows
-# which correspond to duplicate evaluations
-rating <- select(ref.data, cas_id, reference_time_management_rating_0:reference_time_management_rating_3) %>%
-    gather(ref_num, rating, reference_time_management_rating_0:reference_time_management_rating_3, na.rm = TRUE) %>%
-    mutate(ref_num = as.numeric(str_extract(ref_num, "[0-9]"))) %>%
-    inner_join(references, by=c("cas_id", "ref_num")) %>%
-    select(cas_id:rating)
-
-# gather the time management comments into long format, remove those rows which
-# correspond to duplicate evaluations; join with the time management ratings
-time.mgmt <- select(ref.data, cas_id, reference_time_management_rating_comments_0:reference_time_management_rating_comments_3) %>%
-    gather(ref_num, comment, reference_time_management_rating_comments_0:reference_time_management_rating_comments_3, na.rm = TRUE) %>%
-    mutate(ref_num = as.numeric(str_extract(ref_num, "[0-9]")),
-           quality = "Time Management") %>%
-    inner_join(rating, by=c("cas_id", "ref_num"))
-
-# repeat the process for other characteristics
-rating <- select(ref.data, cas_id, reference_problem_solving_rating_0:reference_problem_solving_rating_3) %>%
-    gather(ref_num, rating, reference_problem_solving_rating_0:reference_problem_solving_rating_3, na.rm = TRUE) %>%
-    mutate(ref_num = as.numeric(str_extract(ref_num, "[0-9]"))) %>%
-    inner_join(references, by=c("cas_id", "ref_num")) %>%
-    select(cas_id:rating)
-
-prob.solve <- select(ref.data, cas_id, reference_problem_solving_rating_comments_0:reference_problem_solving_rating_comments_3) %>%
-    gather(ref_num, comment, reference_problem_solving_rating_comments_0:reference_problem_solving_rating_comments_3, na.rm = TRUE) %>%
-    mutate(ref_num = as.numeric(str_extract(ref_num, "[0-9]")),
-           quality = "Problem Solving") %>%
-    inner_join(rating, by=c("cas_id", "ref_num"))
-
-rating <- select(ref.data, cas_id, reference_independence_rating_0:reference_independence_rating_3) %>%
-    gather(ref_num, rating, reference_independence_rating_0:reference_independence_rating_3, na.rm = TRUE) %>%
-    mutate(ref_num = as.numeric(str_extract(ref_num, "[0-9]"))) %>%
-    inner_join(references, by=c("cas_id", "ref_num")) %>%
-    select(cas_id:rating)
-
-independ <- select(ref.data, cas_id, reference_independence_rating_comments_0:reference_independence_rating_comments_3) %>%
-    gather(ref_num, comment, reference_independence_rating_comments_0:reference_independence_rating_comments_3, na.rm = TRUE) %>%
-    mutate(ref_num = as.numeric(str_extract(ref_num, "[0-9]")),
-           quality = "Independence") %>%
-    inner_join(rating, by=c("cas_id", "ref_num"))
-
-rating <- select(ref.data, cas_id, reference_emotional_maturity_rating_0:reference_emotional_maturity_rating_3) %>%
-    gather(ref_num, rating, reference_emotional_maturity_rating_0:reference_emotional_maturity_rating_3, na.rm = TRUE) %>%
-    mutate(ref_num = as.numeric(str_extract(ref_num, "[0-9]"))) %>%
-    inner_join(references, by=c("cas_id", "ref_num")) %>%
-    select(cas_id:rating)
-
-mature <- select(ref.data, cas_id, reference_emotional_maturity_rating_comments_0:reference_emotional_maturity_rating_comments_3) %>%
-    gather(ref_num, comment, reference_emotional_maturity_rating_comments_0:reference_emotional_maturity_rating_comments_3, na.rm = TRUE) %>%
-    mutate(ref_num = as.numeric(str_extract(ref_num, "[0-9]")),
-           quality = "Maturity") %>%
-    inner_join(rating, by=c("cas_id", "ref_num"))
-
-# bind all the characteristics together into a single table
-lor <- bind_rows(time.mgmt, prob.solve, independ, mature) %>%
-    mutate(comment = ifelse(comment == "", NA, comment),
-           quality = factor(quality),
+# gather the ratings into long data format, remove those rows which correspond
+# to duplicate evaluations
+rating <- select(ref.data, cas_id, matches("reference_(.*)_rating_[0-3]$")) %>%
+    gather(ref_num, rating, starts_with("reference_"), na.rm = TRUE) %>%
+    extract(ref_num, c("quality", "ref_num"), "reference_(.*)_rating_([0-3])$") %>%
+    mutate(ref_num = as.numeric(ref_num),
            rating = factor(rating, exclude = "")) %>%
+    inner_join(references, by=c("cas_id", "ref_num")) %>%
+    select(cas_id:rating)
+
+# gather the comments into long data format, remove those rows which correspond
+# to duplicate evaluations
+comments <- select(ref.data, cas_id, matches("reference_(.*)_comments_[0-3]$")) %>%
+    gather(ref_num, comment, starts_with("reference_"), na.rm = TRUE) %>%
+    extract(ref_num, c("quality", "ref_num"), "reference_(.*)_rating_comments_([0-3])$") %>%
+    mutate(ref_num = as.numeric(ref_num)) %>%
+    inner_join(references, by=c("cas_id", "ref_num")) %>%
+    select(cas_id:comment)
+
+# combine rating and comments 
+lor <- inner_join(rating, comments, by=c("cas_id", "quality", "ref_num")) %>%
     arrange(cas_id, quality, ref_num)
+
+rm(rating, comments)
