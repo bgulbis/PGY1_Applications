d47b1d5390dd9e28b7d9d3d56bfe75c95a5ca485
bgulbis <brian.gulbis@gmail.com>
2016-01-09 11:26:20 -0600

    loi section

--- summary_merge.R
+++ summary_merge.R
@@ -5,13 +5,20 @@
 
 library(knitr)
 library(rmarkdown)
+# opts_chunk$set(tidy.opts=list(width.cutoff=60))
 
 if (!exists("applicants")) {
     applicants <- readRDS("applicants.Rds")
 }
 
-for (i in 1:nrow(applicants)) {
-    rmarkdown::render(input = "summary.Rmd", output_format = "pdf_document", 
-                      output_file = paste(applicants$last_name[i], "_", applicants$first_name[i], ".pdf", sep = ""),
-                      output_dir = "summaries")
-}
+# for (i in 1:nrow(applicants)) {
+#     rmarkdown::render(input = "summary.Rmd", output_format = "pdf_document", 
+#                       output_file = paste(applicants$last_name[i], "_", applicants$first_name[i], ".pdf", sep = ""),
+#                       output_dir = "summaries")
+# }
+
+# test
+i <- 1
+rmarkdown::render(input = "summary.Rmd", output_format = "pdf_document", 
+                  output_file = paste(applicants$last_name[1], "_", applicants$first_name[1], ".pdf", sep = ""),
+                  output_dir = "summaries")
