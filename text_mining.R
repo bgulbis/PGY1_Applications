9e54dd9a008fbf07a94d142566d5128e2e64c54f
bgulbis <brian.gulbis@gmail.com>
2016-02-07 16:09:39 -0600

    openNLP, koRpus

--- text_mining.R
+++ text_mining.R
@@ -8,12 +8,14 @@ library(SnowballC)
 library(wordcloud)
 library(sentiment)
 library(openNLP)
+library(koRpus)
 
 # use ref.assess
 if (!exists("ref.assess")) {
     ref.assess <- readRDS("ref.assess.Rds")
 }
 
+# tm----
 # convert text into a corpus
 vs <- VectorSource(ref.assess$comment)
 corpus <- Corpus(vs)
@@ -41,7 +43,7 @@ dtm.dense <- as.matrix(dtm.common)
 freq <- colSums(dtm.dense)
 freq <- sort(freq, decreasing = TRUE)
 
-# make wordcloud
+# wordcloud----
 words <- names(freq)
 wordcloud(words[1:50], freq[1:50])
 
@@ -49,7 +51,7 @@ wordcloud(words[1:50], freq[1:50])
 findFreqTerms(dtm.common, 5)
 findAssocs(dtm.common, "will", 0.7)
 
-# sentiment package
+# sentiment----
 # classify emotion and polarity
 class_emo <- classify_emotion(ref.assess$comment)
 class_pol <- classify_polarity(ref.assess$comment)
@@ -57,10 +59,27 @@ class_pol <- classify_polarity(ref.assess$comment)
 test <- data.frame(emotion = class_emo[, 7], polarity = class_pol[, 4])
 test2 <- bind_cols(ref.assess, test)
 
-# openNLP
+# openNLP----
 # text <- ref.assess %>%
-    # mutate(string.text = as.String(comment))
-text <- as.String(ref.assess$comment)
+#     mutate(string.text = ifelse(!is.na(comment), as.String(comment), NA))
+
+
+text2 <- ref.assess %>%
+    filter(!is.na(comment)) 
+
+str.text <- as.String(text2$comment)
 
 word.ann <- Maxent_Word_Token_Annotator()
 sent.ann <- Maxent_Sent_Token_Annotator()
+
+my.text <- annotate(str.text, list(sent.ann, word.ann))
+
+pos.ann <- Maxent_POS_Tag_Annotator()
+
+my.text2 <- annotate(str.text, pos.ann, my.text)
+
+# my.sent <- sents(my.text)
+
+# koRpus----
+write.table(text2$comment, file = file("rec_text.txt", "wb"), row.names = FALSE, col.names = FALSE)
+tagged.txt <- treetag("rec_text.txt", treetagger = "C:/TreeTagger/bin/tag-english.bat", lang = "en")
