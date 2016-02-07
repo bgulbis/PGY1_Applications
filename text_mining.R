5f997241a7a8798739018588c503bf2c2d2ae52c
bgulbis <brian.gulbis@gmail.com>
2016-02-06 21:27:34 -0600

    sentiment package; start openNLP

--- text_mining.R
+++ text_mining.R
@@ -6,6 +6,8 @@ library(dplyr)
 library(tm)
 library(SnowballC)
 library(wordcloud)
+library(sentiment)
+library(openNLP)
 
 # use ref.assess
 if (!exists("ref.assess")) {
@@ -29,7 +31,7 @@ corpus <- tm_map(corpus, stripWhitespace)
 # create document term matrix
 # tdm <- TermDocumentMatrix(corpus)
 dtm <- DocumentTermMatrix(corpus)
-dtm.dict <- DocumentTermMatrix(corpus, list(dictionary = c("improv", "benefi")))
+# dtm.dict <- DocumentTermMatrix(corpus, list(dictionary = c("improv", "benefi")))
 
 # inspect(dtm)
 
@@ -46,3 +48,19 @@ wordcloud(words[1:50], freq[1:50])
 # find frequent terms
 findFreqTerms(dtm.common, 5)
 findAssocs(dtm.common, "will", 0.7)
+
+# sentiment package
+# classify emotion and polarity
+class_emo <- classify_emotion(ref.assess$comment)
+class_pol <- classify_polarity(ref.assess$comment)
+
+test <- data.frame(emotion = class_emo[, 7], polarity = class_pol[, 4])
+test2 <- bind_cols(ref.assess, test)
+
+# openNLP
+# text <- ref.assess %>%
+    # mutate(string.text = as.String(comment))
+text <- as.String(ref.assess$comment)
+
+word.ann <- Maxent_Word_Token_Annotator()
+sent.ann <- Maxent_Sent_Token_Annotator()
